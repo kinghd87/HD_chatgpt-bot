761499c2132453616a3a47a674382e7e71ad4942
kinghd87 <fearlesssoul31@gmail.com>
2025-09-05 18:24:05 +0500

    Update info.py

--- info.py
+++ info.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = environ.get("API_ID" , "7409967984")
-API_HASH = environ.get("API_HASH" , "b1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
+API_ID = environ.get("API_ID" , "23340169")
+API_HASH = environ.get("API_HASH" , "4e600e23e419b9cffd280ee2791c273a")
 BOT_TOKEN = environ.get("BOT_TOKEN" , "8217509079:AAEY3LgU3o4W36mOhAURCN0xjVipK7u-ofc")
-ADMIN = int(environ.get("ADMIN" , "1847899007"))
-CHAT_GROUP = int(environ.get("CHAT_GROUP", "-100"))
-LOG_CHANNEL = environ.get("LOG_CHANNEL", "-100")
+ADMIN = int(environ.get("ADMIN" , "7409967984"))
+CHAT_GROUP = int(environ.get("CHAT_GROUP", "-1003002158481"))
+LOG_CHANNEL = environ.get("LOG_CHANNEL", "-1002569365457")
 MONGO_URL = environ.get("MONGO_URL" , "mongodb://localhost:27017")
 AUTH_CHANNEL = int(
     environ.get("AUTH_CHANNEL", "-1001734958816")
@@ -23,4 +23,5 @@ ONLY_SCAN_IN_GRP = environ.get(
     "ONLY_SCAN_IN_GRP", True
 )  # If IMG_SCAN_IN_GRP is set to True, image scanning is restricted to your support group only. If it's False, the image scanning feature can be used anywhere.
 REACTIONS = ["❤️‍🔥", "⚡", "🔥"]
+
 
